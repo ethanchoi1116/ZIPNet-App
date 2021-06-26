d1948bc4f5e5d383c80c0cc46bf110e90473fb76
Yeonsoo Choi <yschoi6307@gmail.com>
2021-06-27 00:39:05 +0900

    api server to ec2

--- app/main.py
+++ app/main.py
@@ -50,4 +50,4 @@ async def predict(file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    uvicorn.run("main:app", host="0.0.0.0", port=5000)
