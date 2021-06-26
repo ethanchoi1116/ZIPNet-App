dc38239e259bd166027c309fd6232371b5aef033
Yeonsoo Choi <yschoi6307@gmail.com>
2021-06-27 00:03:49 +0900

    api uri

--- app/main.py
+++ app/main.py
@@ -50,4 +50,4 @@ async def predict(file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", default=8000)))
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
