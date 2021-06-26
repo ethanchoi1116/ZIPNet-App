da598a451e1cfd61e78e9e5007144b144416c8e6
Yeonsoo Choi <yschoi6307@gmail.com>
2021-06-27 01:41:14 +0900

    back to heroku

--- app/main.py
+++ app/main.py
@@ -52,4 +52,6 @@ async def predict(file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    uvicorn.run(
+        "main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), workers=1
+    )
